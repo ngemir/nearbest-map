1f8b4c13b297cca7219a49f02e6bc46fb7f79546
Emir <ngemir@gmail.com>
2019-03-02 00:36:26 -0300

    Updates

---
+++ precache-manifest.f31c34ece173a9cd83a333cbbda927a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "04aa0daedda7940dc87ac23ebfa1d459",
+    "url": "/nearbest-map/static/media/loader.04aa0dae.gif"
+  },
+  {
+    "revision": "e7dda5140f3cd2f2fe545fe6affc9faa",
+    "url": "/nearbest-map/static/media/foursquare.e7dda514.png"
+  },
+  {
+    "revision": "7ea6d0e84ccdc0b3c914",
+    "url": "/nearbest-map/static/js/runtime~main.7ea6d0e8.js"
+  },
+  {
+    "revision": "4d8d4793cd91abaf9539",
+    "url": "/nearbest-map/static/js/main.4d8d4793.chunk.js"
+  },
+  {
+    "revision": "50cbcc95259828c4d06b",
+    "url": "/nearbest-map/static/js/2.50cbcc95.chunk.js"
+  },
+  {
+    "revision": "4d8d4793cd91abaf9539",
+    "url": "/nearbest-map/static/css/main.f33f437d.chunk.css"
+  },
+  {
+    "revision": "5e7e81c26c890a4d13a5d632ad525d7a",
+    "url": "/nearbest-map/index.html"
+  }
+];
